910c9c418463dd36d70425c32bc9d59165125ff9
syedfahdali <syedfahd999@gmail.com>
2023-05-22 10:36:39 +0500

    Making first React Native App

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+export default Home;
