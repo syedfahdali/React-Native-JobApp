2556581268b980ff0a807379254e75f72de005c4
syedfahdali <syedfahd999@gmail.com>
2023-05-22 11:04:28 +0500

    Added some header files

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { useState } from "react";
-
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 const Home = () => {
   return (
     <View>
